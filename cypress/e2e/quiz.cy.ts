6ae8fcfc2a02ff53dd13291af000c9affca0cf19
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-11-09 17:43:16 -0500

    completed e2e test for quiz

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,5 +1,51 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Quiz User Journey', () => {
+  it('A user can visit the page to start the quiz', () => {
+    cy.visit('http://localhost:3001/')
+  });
+
+  it('A user can start the quiz, click any of the 4 answers, see following questions, and get a final score', () => {
+    // visit quiz page
+    cy.visit('http://localhost:3001/')
+    // start quiz
+    cy.get('button').should('exist').should('have.text', 'Start Quiz').click();
+    // click answers through the questions
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(0).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(2).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(1).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(3).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(1).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(2).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(0).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(3).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(3).click();
+    cy.get('.mt-3')
+      .find('.btn.btn-primary').eq(0).click();
+
+    // should see quiz completed, score, and button to take new quiz
+    cy.get('.card.p-4.text-center').should('exist')
+      .find('h2').should('exist').and('include.text', 'Quiz Completed');
+
+    cy.get('.card.p-4.text-center')
+      .find('.alert.alert-success').should('exist').and('include.text', 'Your score:');
+    
+    // clicking take new quiz button
+    cy.get('.card.p-4.text-center')
+      .find('button').should('exist').and('have.text', 'Take New Quiz').click();
+
+    // should see first questions and 4 answers
+    cy.get('.card.p-4').should('exist')
+      .find('h2').should('include.text', '?').should('exist');
+    cy.get('.card.p-4').should('exist')
+      .find('.mt-3').should('exist')
+      .find('.d-flex.align-items-center.mb-2').should('exist').should('have.length', 4);
+  });
 })
