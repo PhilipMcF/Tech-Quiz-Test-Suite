a403848c4686229dddd6af3141bdb33f080dea73
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-11-08 19:05:51 -0500

    create test for Quiz component ; tests if component mounts, renders buttons, questions, answers.

--- cypress/component/Quiz.cy.tsx
+++ cypress/component/Quiz.cy.tsx
@@ -1,5 +1,36 @@
+import React from "react";
+import Quiz from "../../client/src/components/Quiz";
+
+
 describe('Quiz.cy.tsx', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
+  it('should render the start quiz button', () => {
+    cy.mount(<Quiz />)
+
+    // start quiz button
+    cy.get('button').should('exist').should('have.text', 'Start Quiz')
+  });
+
+  it('should render start quiz button, then render sameple question', () => {
+    cy.mount(<Quiz />)
+
+    // start quiz button
+    cy.get('button').should('exist').should('have.text', 'Start Quiz').click()
+
+    // question card + h2 with question mark
+    cy.get('.card.p-4').should('exist')
+      .find('h2').should('include.text', '?').should('exist')
+
+    // question card with 4 buttons
+    cy.get('.card.p-4').should('exist')
+      .find('.mt-3').should('exist')
+      .find('.d-flex.align-items-center.mb-2').should('exist')
+      .find('.btn.btn-primary').should('have.length', 4).should('exist')
+
+    // question card with  4 answers
+    cy.get('.card.p-4').should('exist')
+      .find('.mt-3').should('exist')
+      .find('.d-flex.align-items-center.mb-2').should('exist')
+      .find('.alert.alert-secondary.mb-0.ms-2.flex-grow-1').should('have.length', 4).should('exist')
+
+  });
 })
